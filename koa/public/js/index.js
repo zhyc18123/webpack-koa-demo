f41130eae708b95f7f94be2df4e1b86684b7d837
zhangyoucong <zhangyoucong@ipin.com>
2016-12-17 09:44:15 +0800

    up

--- koa/public/js/index.js
+++ koa/public/js/index.js
@@ -46,7 +46,7 @@
 
 	"use strict";
 
-	__webpack_require__(4);
+	__webpack_require__(1);
 	$(function () {
 		var init = function init() {
 			var xinSwiper = new Swiper('.swiper-container', {
@@ -59,10 +59,7 @@
 	});
 
 /***/ },
-/* 1 */,
-/* 2 */,
-/* 3 */,
-/* 4 */
+/* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
