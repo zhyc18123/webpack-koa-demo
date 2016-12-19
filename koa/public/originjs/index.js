7eca5463a0c782c0c7d3859ba4cf77fd3fe3618d
zhangyoucong <zhangyoucong@ipin.com>
2016-12-19 18:10:11 +0800

    up

--- koa/public/originjs/index.js
+++ koa/public/originjs/index.js
@@ -2,11 +2,34 @@
 require("./utils/swiper/swiper-3.3.1.jquery.min");
 require('./analysis-report');
 var getVip=require("./get-vip");
+var input=require("./input");
 $(function(){
 	var init=function(){
-		var xinSwiper = new Swiper ('.swiper-container', {
+		// var swiperHeight=[];
+		var xinSwiper = new Swiper ('.xin-con', {
+			autoHeight:true,
+			onSlideChangeStart: function() {
+				var activeSlide=$(".swiper-slide").eq(xinSwiper.activeIndex);
+				console.log(activeSlide.height())
+				if(activeSlide.height()<$(window).height()){
+					activeSlide.css('height', $(window).height() + 'px');
+					$(".swiper-wrapper").css('height', $(window).height() + 'px');
+				}else{
+					$(".swiper-wrapper").css('height', activeSlide.height() + 'px');
+				};
+			},
+			onInit: function(swiper){
+				console.log($(".swiper-slide").eq(0).height())
+				console.log($(window).height())
+				if($(".swiper-slide").eq(0).height()<$(window).height()){
+					console.log("xdd")
+					$(".swiper-slide").eq(0).css('height', $(window).height() + 'px');
+					$(".swiper-wrapper").css('height', $(window).height() + 'px');
+				};
+			}
 		});
 		getVip.init(xinSwiper);
+		input.init(xinSwiper);
 	};
 	init();
 });
---
+++ koa/public/originjs/input.js
@@ -0,0 +1,17 @@
+import anaReport from "./";
+var init=function(xinSwiper){
+	$("#get-report").on("click",function(){
+		var data={
+			req_id:"",
+			exam_no:"",
+			province_id:"",
+			score:"",
+			exp_sch_id:"",
+			batch:"",
+		};
+
+	});
+};
+module.exports = {
+    init:init
+}
