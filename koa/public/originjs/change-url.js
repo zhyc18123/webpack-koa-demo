e1b548c393915a5994c3a8fc33ac2090d50bf58c
zhangyoucong <zhangyoucong@ipin.com>
2016-12-27 14:51:13 +0800

    up

--- koa/public/originjs/change-url.js
+++ koa/public/originjs/change-url.js
@@ -3,7 +3,7 @@
 var changeUrl = function(state, title, url) {
 	history.go(+1);
 	if ('pushState' in history) {
-		history.pushState(state, title, "/score/analyse"+url);
+		history.pushState(state, title, "/score/analyse?p="+state+url);
 	};
 };
 module.exports={
